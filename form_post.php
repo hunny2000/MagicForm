7d93a46332215ab904f27005efe1b567a3825b99
hunny2000 <51091996+hunny2000@users.noreply.github.com>
2022-02-24 23:28:10 +0530

    post

--- form_post.php
+++ form_post.php
@@ -1,15 +1,16 @@
 <?php
-  $highscoreFile = './highscore.txt'; 
-  $Highscore = (int) file_get_contents($highscoreFile);
-  $Score = (int) $_POST["ScoreKey"];
-
-  if($Score > $Highscore)
-  {
-    file_put_contents ($highscoreFile, $Score);
-    echo "New high score updated successfully\nHigh score is: " . $Score;
-  }
-  else
-  {
+      $highscoreFile = './highscore.txt'; 
+      $Highscore = (int) file_get_contents($highscoreFile);
+      $Score = (int) $_POST["ScoreKey"];
       echo $Score ." is not a new high score\nHigh score is: ". $Highscore;
-  }
+
+      if($Score > $Highscore)
+      {
+            file_put_contents ($highscoreFile, $Score);
+            echo "New high score updated successfully\nHigh score is: " . $Score;
+      }
+      else
+      {
+        
+      }
 ?>
